24f93c89a0d57b0d5ef499bdee46cf8d6c951309
America Viana <america.viana@AMCNETWORKS.COM>
2023-03-19 16:12:07 -0300

    2do ejercicio

---
+++ Ejercicio2lista.js
@@ -0,0 +1,26 @@
+// Array
+
+const nombre = ["America"];
+console.log(nombre)
+
+const edad = [33];
+console.log(edad)
+
+const desarrollador = true;
+console.log(desarrollador)
+
+// Fecha
+
+const fecha_nacimiento = new Date(1989, 9, 08);
+console.log(fecha_nacimiento)
+
+// Objetos
+
+const libro_favorito = {
+    titulo: "Cien Años De Soledad",
+    autor: "Gabriel Garcia Marquez",
+    fecha: new Date ("march 05 1967"),
+}
+
+console.log(libro_favorito)
+
